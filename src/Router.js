a7c7437e195ca19e6d53b3e34d05b6e9afd589a3
jambottle <jaewon.walks@gmail.com>
2022-01-18 12:55:49 +0900

    Add: Router.js & Main.js, Nav.js 추가

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Main from './pages/Main';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
