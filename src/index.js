a3b41b5a0f2afe977a91b4b1813119b2c4427999
jambottle <jaewon.walks@gmail.com>
2022-01-18 23:50:17 +0900

    Chore: styled-bootstrap-grid & Font Awesome 설치 - 반응형 웹 설계 및 Grid System 구현을 위해 CSS 라이브러리 활용 - 범용 아이콘 셋을 통한 직관적 UI 구현을 위해 CSS 기반 툴킷 활용

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyle from './styles/GlobalStyle';
+import { BaseCSS } from 'styled-bootstrap-grid';
 import Router from './Router';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
+    <BaseCSS />
     <Router />
   </React.StrictMode>,
   document.getElementById('root')
