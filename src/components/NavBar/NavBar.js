c2beb54b8fb21aacf06c896605f037c8e2e8cf80
jambottle <jaewon.walks@gmail.com>
2022-01-19 14:25:26 +0900

    Modify: NavBar 컴포넌트 레이아웃 및 스타일링 일부 수정 - NavBar의 상단 고정으로 인해 빈 공간이 발생하는 문제를 NavBumper 컴포넌트로 해결 - border 색상 및 관련 스타일링 컨벤션 통일 & 가독성 향상을 위한 Line Spacing 실시

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,59 +6,62 @@ import NAV_LIST_DATA from './navBarData';
 
 export default function GlobalNavBar() {
   return (
-    <NavContainer>
-      <Container>
-        <NavRow>
-          <NavDropdown col={2}>
-            <button type="button">
-              <i className="far fa-bars" />
-            </button>
-            <Link to="/">
-              <img
-                alt="Wanted Logo"
-                src="https://file.irgo.co.kr/data/IRPAGE/IMG/J5JBJDM4628QENCPSXV5PS4QN9CUWYZU.svg"
-              />
-            </Link>
-          </NavDropdown>
+    <>
+      <NavContainer>
+        <Container>
+          <NavRow>
+            <NavDropdown col={2}>
+              <button type="button">
+                <i className="far fa-bars" />
+              </button>
+              <Link to="/">
+                <img
+                  alt="Wanted Logo"
+                  src="https://file.irgo.co.kr/data/IRPAGE/IMG/J5JBJDM4628QENCPSXV5PS4QN9CUWYZU.svg"
+                />
+              </Link>
+            </NavDropdown>
 
-          <NavLinkList col={7}>
-            <nav>
-              <ul>
-                {NAV_LIST_DATA.map(data => {
-                  const { id, title, url, status } = data;
-                  return (
-                    <li key={id}>
-                      <NavLink to={url}>{title}</NavLink>
-                      <span>{status}</span>
-                    </li>
-                  );
-                })}
-              </ul>
-            </nav>
-          </NavLinkList>
+            <NavLinkList col={7}>
+              <nav>
+                <ul>
+                  {NAV_LIST_DATA.map(data => {
+                    const { id, title, url, status } = data;
+                    return (
+                      <li key={id}>
+                        <NavLink to={url}>{title}</NavLink>
+                        <span>{status}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </nav>
+            </NavLinkList>
 
-          <NavFeatures col={3}>
-            <button type="button">
-              <i className="far fa-search" />
-            </button>
-            <button type="button">
-              <i className="far fa-bell" />
-              <span>N</span>
-            </button>
-            <button type="button" className="userFeature">
-              <img
-                alt="User Profile"
-                src="https://images.unsplash.com/photo-1641391503184-a2131018701b"
-              />
-              <span>N</span>
-            </button>
-            <button type="button" className="corpFeature">
-              <Link to="/dashboard/companyregister">기업 서비스</Link>
-            </button>
-          </NavFeatures>
-        </NavRow>
-      </Container>
-    </NavContainer>
+            <NavFeatures col={3}>
+              <button type="button">
+                <i className="far fa-search" />
+              </button>
+              <button type="button">
+                <i className="far fa-bell" />
+                <span>N</span>
+              </button>
+              <button type="button" className="userFeature">
+                <img
+                  alt="User Profile"
+                  src="https://images.unsplash.com/photo-1641391503184-a2131018701b"
+                />
+                <span>N</span>
+              </button>
+              <button type="button" className="corpFeature">
+                <Link to="/dashboard/companyregister">기업 서비스</Link>
+              </button>
+            </NavFeatures>
+          </NavRow>
+        </Container>
+      </NavContainer>
+      <NavBumper />
+    </>
   );
 }
 
@@ -67,8 +70,9 @@ const NavContainer = styled.div`
   top: 0;
   left: 0;
   z-index: 5;
+
   width: 100%;
-  border-bottom: 1px solid #ebebeb;
+  border-bottom: 1px solid #e1e2e3;
   background-color: #ffffff;
 `;
 
@@ -81,7 +85,7 @@ const NavRow = styled(Row)`
 const NavDropdown = styled(Col)`
   button {
     margin-right: 10px;
-    border: 0;
+    border: none;
     background: none;
     cursor: pointer;
 
@@ -123,6 +127,7 @@ const NavLinkList = styled(Col)`
         position: absolute;
         top: 8px;
         right: -6px;
+
         color: #3366ff;
         font-size: 10px;
         cursor: default;
@@ -137,7 +142,7 @@ const NavFeatures = styled(Col)`
 
   button {
     position: relative;
-    border: 0;
+    border: none;
     padding: 0 10px;
     background: none;
     cursor: pointer;
@@ -152,10 +157,12 @@ const NavFeatures = styled(Col)`
       position: absolute;
       top: -1px;
       right: 0;
+
       width: 12px;
       height: 13px;
       border-radius: 4px;
       padding-top: 1px;
+
       background-color: #3366ff;
       color: #ffffff;
       font-size: 10px;
@@ -182,10 +189,12 @@ const NavFeatures = styled(Col)`
         position: absolute;
         top: -2px;
         right: -4px;
+
         width: 12px;
         height: 13px;
         border-radius: 4px;
         padding-top: 1px;
+
         background-color: #3366ff;
         color: #ffffff;
         font-size: 10px;
@@ -214,6 +223,7 @@ const NavFeatures = styled(Col)`
         margin-left: 20px;
         border: 1px solid #e1e2e3;
         border-radius: 20px;
+
         color: #666666;
         font-size: 13px;
         font-weight: 400;
@@ -221,4 +231,11 @@ const NavFeatures = styled(Col)`
       }
     }
   }
+`;
+
+const NavBumper = styled(Row)`
+  width: 100%;
+  height: 75px;
+  margin: 0;
+  background: none;
 `;
