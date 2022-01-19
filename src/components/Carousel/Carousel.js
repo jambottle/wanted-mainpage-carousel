23f0aeb7c4bedc3cc8ee8d633c12f72028896837
jambottle <jaewon.walks@gmail.com>
2022-01-20 06:30:31 +0900

    Add: Carousel 컴포넌트 완성 (Carousel.js) - Infinite Carousel 컴포넌트의 레이아웃 및 기본적인 기능 구현 - Array Data의 Index를 기준으로 자동/수동 슬라이드 이벤트 탑재 - 슬라이드 이벤트 발생 시, 간단한 CSS Transition 효과 설정

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,18 +1,99 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Row } from 'styled-bootstrap-grid';
 import CarouselCard from './CarouselCard';
 import CARD_LIST_DATA from './carouselData';
 
-export default function Carousel() {
+const AUTO_TRANSITION_DURATION = 4000;
+const SELF_TRANSITION_DURATION = 1000;
+
+export default function InfiniteCarousel() {
+  const [prevIndex, setPrevIndex] = useState(1);
+
+  const slideLeft = () => {
+    const nextIndex = prevIndex - 1;
+    setPrevIndex(nextIndex);
+
+    if (nextIndex === 0) {
+      setTimeout(() => {
+        setPrevIndex(CARD_LIST_DATA.length);
+      }, SELF_TRANSITION_DURATION);
+    }
+  };
+
+  const slideRight = () => {
+    const nextIndex = prevIndex + 1;
+    setPrevIndex(nextIndex);
+
+    if (nextIndex === CARD_LIST_DATA.length + 1) {
+      setTimeout(() => {
+        setPrevIndex(1);
+      }, SELF_TRANSITION_DURATION);
+    }
+  };
+
+  useEffect(() => {
+    console.log(prevIndex);
+    const interval = setInterval(() => {
+      slideRight();
+    }, AUTO_TRANSITION_DURATION);
+    return () => clearInterval(interval);
+  }, [prevIndex]);
+
   return (
     <CarouselWrapper>
-      {CARD_LIST_DATA.map(data => {
-        const { id } = data;
-        return <CarouselCard key={id} data={data} />;
-      })}
+      <CarouselCardList
+        style={{
+          transform: `translateX(-${prevIndex * 100}%)`,
+          transition: `all ${SELF_TRANSITION_DURATION}ms ease-in-out`,
+        }}
+      >
+        <CarouselCard data={CARD_LIST_DATA[CARD_LIST_DATA.length - 1]} />
+        {CARD_LIST_DATA.map((data, index) => {
+          return <CarouselCard key={index} data={data} />;
+        })}
+        <CarouselCard data={CARD_LIST_DATA[0]} />
+      </CarouselCardList>
+
+      <button className="leftButton" onClick={slideLeft}>
+        <i className="fal fa-chevron-left" />
+      </button>
+      <button className="rightButton" onClick={slideRight}>
+        <i className="fal fa-chevron-right" />
+      </button>
     </CarouselWrapper>
   );
 }
 
-const CarouselWrapper = styled(Row)``;
+const CarouselWrapper = styled.div`
+  position: relative;
+  width: 1140px;
+  height: 320px;
+  margin: 0 auto;
+
+  button {
+    position: absolute;
+    top: 120px;
+
+    width: 30px;
+    height: 60px;
+    border: none;
+    border-radius: 20px;
+
+    background-color: #ebebeb;
+    opacity: 0.6;
+    cursor: pointer;
+
+    &.leftButton {
+      left: -60px;
+    }
+
+    &.rightButton {
+      right: -60px;
+    }
+  }
+`;
+
+const CarouselCardList = styled.section`
+  display: flex;
+  height: 100%;
+`;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import { Container } from 'styled-bootstrap-grid';
 import Carousel from '../components/Carousel/Carousel';
 
 export default function Main() {
-  return (
-    <Container>
-      <Carousel />
-    </Container>
-  );
+  return <Carousel />;
 }
